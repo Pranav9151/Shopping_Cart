4de4e0ae7c8de7a0e25d813e8e0ebac6e046ccf4
Pranav <Pranav@Pranav>
2022-07-25 20:03:10 +0530

    initial commit

---
+++ ShoppingCart_Discovery-MS/src/test/java/com/cg/eureka/discovery/ShoppingCartDiscoveryMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.eureka.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartDiscoveryMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
