c44a370fe3c2b285b715a3fec3350762cd08220d
Pranav <Pranav@Pranav>
2022-07-25 20:05:04 +0530

    commit

---
+++ ShoppingCart_ProfileService/src/main/java/com/cg/profileservice/customercontroller/CustomerController.java
@@ -0,0 +1,18 @@
+package com.cg.profileservice.customercontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class CustomerController {
+
+	
+	@GetMapping("/status")
+	public String status() 
+	{
+		return "Hello Sir!";
+	}
+	
+}
