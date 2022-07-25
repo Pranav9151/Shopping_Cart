9fe7d0392f06d42383c5a1f094b474668b68d471
Pranav <Pranav@Pranav>
2022-07-25 20:15:51 +0530

    initial

---
+++ ShoppingCart_CartService/src/test/java/com/cg/cartservice/ShoppingCartCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.cartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
